51caa2cac55a9833954e2d65e9586f357d4dba78
Nelson Álvarez <nelsonalvarez_0118@outlook.es>
2026-08-17 20:46:09 -0300

    Gitleaks lab: agregar configuracion AWS

--- gitleaks-lab/config.js
+++ gitleaks-lab/config.js
@@ -1,6 +1,11 @@
 const config = {
   environment: "development",
-  region: "us-east-1"
+  region: "us-east-1",
+
+  aws: {
+    accessKeyId: "AKIAIOSFODNN7EXAMPLE",
+    secretAccessKey: "aB3cD4eF5gH6iJ7kL8mN9oP0qR1sT2uV3wX4yZ"
+  }
 };
 
 module.exports = config;
